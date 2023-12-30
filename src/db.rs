aa54301097d33a4a33d6725a9dde06ea71a5cd58
Markus Paasiniemi <github@2pi.fi>
2023-12-30 12:43:34 +0200

    initial commit

---
+++ src/db.rs
@@ -0,0 +1,13 @@
+use crate::err::Res;
+use sqlx::sqlite::SqliteConnectOptions;
+use sqlx::{migrate, ConnectOptions, SqliteConnection};
+
+pub async fn get_connection(db_path: &str) -> Res<SqliteConnection> {
+    let opt = SqliteConnectOptions::new()
+        .filename(db_path)
+        .create_if_missing(true);
+    let mut con = opt.connect().await?;
+
+    migrate!().run(&mut con).await?;
+    Ok(con)
+}
